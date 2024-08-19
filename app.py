5f66f34325dada849428bfe6e0f49e291fe057a4
ethicsense <gycksdl94@gmail.com>
2024-08-19 15:37:34 +0900

    edit path

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 '''
 YOLO 결과 화면 구현 
 '''
-import sys
-import io
 import argparse
+import gradio as gr
 import cv2
 import os
 import time 
 import ffmpeg
-
+from ultralytics import YOLO
+import sys
+import io
+import contextlib
+import numpy as np
 import pandas as pd
 from datetime import datetime
-import numpy as np
-
-from ultralytics import YOLO
-import gradio as gr
 
 '''
 object_name 변수
  : 맨 밑에 makdowon과 Model에서 사용합니다. 
 '''
-object_name = "Edge AI NewsCaster"
+object_name = "Edge AI FashionCaster"
 stop_execution = False # Global flag to control execution
 
 def stop_functions():
@@ -63,17 +62,17 @@ sys.stdout = capture_stream
 def get_captured_output():
     return capture_stream.get_output()
 
-# yolo 실행 
+
+## yolo 실행
 def run_yolo(input_video_url):
     global stop_execution
-    stop_execution = False
-    # YOLO 모델 지정 
+    stop_execution = False  # Reset the flag at the beginning of the function
     model = YOLO(f'./weights/' + os.listdir('./weights/')[0])
 
     cap = cv2.VideoCapture(input_video_url)
     w = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(cap.get(cv2.CAP_PROP_FPS)) 
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     video_name = os.path.basename(input_video_url)
     output_path = os.getcwd()+'/video/out/' + video_name
@@ -107,8 +106,9 @@ def run_yolo(input_video_url):
     df.to_csv('detect_log_test.csv', index=False)
     cap.release()
     out.release()
-
-    return input_video_url, resized_video
+    # resized_video = 'video/resized_/' + time.strftime('%Y%m%d%H%M') + '.mp4'
+    # ffmpeg.input(output_path).output(resized_video, crf=35).run() # 영상 용량 축소
+    return input_video_url, output_path
 
 
 if __name__ == "__main__":
@@ -127,17 +127,22 @@ if __name__ == "__main__":
 
     # Gradio UI
     with gr.Blocks() as demo:
+
         with gr.Row():
+
             with gr.Column():
                 markdown = gr.Markdown(f"# {object_name}")
-                input1 = gr.Textbox(label = "Video URL", value="http://evc.re.kr:20096/www/news.mp4") # Video URL 넣기 
+                input1 = gr.Textbox(label = "Video URL", value="http://evc.re.kr:20096/www/test_data/v4_demo1.mp4") # Video URL 넣기 
                 btn1 = gr.Button("Run", size="sm")
-                btn_stop = gr.Button("Stop", size="sm")
+                btn_stop = gr.Button("Stop", size='sm')
+
             with gr.Column():
-                output1 = gr.Video(autoplay=True) # 원본 비디오 재생 
+                output1 = gr.Video(autoplay=True) # 원본 비디오 재생
+
             with gr.Column():
-                output2 = gr.Video(autoplay=True) # 결과 비디오 재생 
-            btn1.click(fn = run_yolo, inputs=input1, outputs=[output1, output2])
+                output2 = gr.Video(autoplay=True) # 결과 비디오 재생
+             
+            btn1.click(fn=run_yolo, inputs=input1, outputs=[output1, output2])
             btn_stop.click(fn=stop_functions)
 
         output_text = gr.Textbox(label="Progress Output", lines=20)
